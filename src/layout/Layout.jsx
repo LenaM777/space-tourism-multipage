8f550408fca964ec4ffe87b8267e53d57aef70aa
Olena Mustafina <toysamiy.mail@gmail.com>
2025-12-20 14:13:52 +0200

    feat: Add dynamic page based on current route

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,10 +1,12 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "../components/Header/Header";
 import "../styles/global.scss";
 
 export default function Layout() {
+  const location = useLocation();
+  const route = location.pathname.split("/")[1] || "home";
   return (
-    <div className="app-wrapper">
+    <div className={`app-wrapper ${route}`}>
       <div className="container">
         <Header />
         <main>
