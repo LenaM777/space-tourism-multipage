609fdbbd0440042cc3a89d40fcec14f96aa4659a
Olena Mustafina <toysamiy.mail@gmail.com>
2025-12-21 14:59:11 +0200

    feat: Add Header include navigation, icons, responsive and styles and create nav-link component, add fonts cdn

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,34 @@
+import { NavLink } from "react-router-dom";
 import "./Header.scss";
+import logo from "/assets/icons/logo.svg";
+import burger from "/assets/icons/icon-hamburger.svg";
 
-const Header = () => {
+export default function Header() {
   return (
-    <div>
-      <a href="/">home</a>
-      <a href="/destination">destination</a>
-      <a href="/crew">crew</a>
-      <a href="/technology">technology</a>
-    </div>
+    <header className="header">
+      <div className="header__elems">
+        <a href="/">
+          <img className="header__logo" src={logo} alt="logo" />
+        </a>
+        <div className="header__line"></div>
+      </div>
+      <nav className="header__nav">
+        <NavLink to="/" className="nav-link">
+          <span>00</span>home
+        </NavLink>
+        <NavLink to="/destination" className="nav-link">
+          <span>01</span>destination
+        </NavLink>
+        <NavLink to="/crew" className="nav-link">
+          <span>02</span>crew
+        </NavLink>
+        <NavLink to="/technology" className="nav-link">
+          <span>03</span>technology
+        </NavLink>
+      </nav>
+      <button className="header__burger">
+        <img src={burger} alt="burger" />
+      </button>
+    </header>
   );
-};
-
-export default Header;
+}
