2829793641c5cc20d7386ef43f9408509022b10b
Olena Mustafina <toysamiy.mail@gmail.com>
2025-12-24 13:23:19 +0200

    feat: Add mobile menu with icons, navigation, styles includes adaptive and update nav-link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import { NavLink } from "react-router-dom";
 import "./Header.scss";
 import logo from "/assets/icons/logo.svg";
 import burger from "/assets/icons/icon-hamburger.svg";
+import close from "/assets/icons/icon-close.svg";
 
 export default function Header() {
   return (
@@ -13,22 +14,41 @@ export default function Header() {
         <div className="header__line"></div>
       </div>
       <nav className="header__nav">
-        <NavLink to="/" className="nav-link">
+        <NavLink to="/" className="nav-link nav-link__desk">
           <span>00</span>home
         </NavLink>
-        <NavLink to="/destination" className="nav-link">
+        <NavLink to="/destination" className="nav-link nav-link__desk">
           <span>01</span>destination
         </NavLink>
-        <NavLink to="/crew" className="nav-link">
+        <NavLink to="/crew" className="nav-link nav-link__desk">
           <span>02</span>crew
         </NavLink>
-        <NavLink to="/technology" className="nav-link">
+        <NavLink to="/technology" className="nav-link nav-link__desk">
           <span>03</span>technology
         </NavLink>
       </nav>
       <button className="header__burger">
         <img src={burger} alt="burger" />
       </button>
+      <div className="mobile-menu">
+        <button className="mobile-menu__btn-close">
+          <img className="mobile-menu__close" src={close} alt="close" />
+        </button>
+        <div className="mobile-menu__nav">
+          <NavLink to="/" className="nav-link nav-link__mobile">
+            <span>00</span>home
+          </NavLink>
+          <NavLink to="/destination" className="nav-link nav-link__mobile">
+            <span>01</span>destination
+          </NavLink>
+          <NavLink to="/crew" className="nav-link nav-link__mobile">
+            <span>02</span>crew
+          </NavLink>
+          <NavLink to="/technology" className="nav-link nav-link__mobile">
+            <span>03</span>technology
+          </NavLink>
+        </div>
+      </div>
     </header>
   );
 }
